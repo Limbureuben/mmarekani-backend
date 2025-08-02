880c196ca1a9353398f3add44c71e3c1682ab95d
Limbureuben <limbureuben4@gmail.com>
2025-08-02 15:35:17 +0300

    changes

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-admin.site.register(CustomUser, UserAdmin)
+# # Register your models here.
+# admin.site.register(CustomUser, UserAdmin)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('id', 'username', 'phone', 'national_id', 'is_staff')
+
+admin.site.register(CustomUser, CustomUserAdmin)
